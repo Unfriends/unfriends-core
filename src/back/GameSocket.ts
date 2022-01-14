42f12c4286e730ea2d72287f9bd053d439a4926a
Eteckq <yohan.g99@gmail.com>
2022-01-15 00:41:37 +0100

    handle events

--- src/back/GameSocket.ts
+++ src/back/GameSocket.ts
@@ -1,4 +1,4 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { RoomsHandler } from "./RoomsHandler";
 import { instrument } from "@socket.io/admin-ui";
 import { createServer } from "http";
@@ -8,28 +8,37 @@ export abstract class GameSocket {
     private rooms = new RoomsHandler()
     protected isMatchmakerUp = false
 
+    private server
+
     constructor(PORT: number, options?: { origin?: string[], debug?: boolean }) {
         const httpServer = createServer();
 
         options = { origin: [], debug: false, ...options }
 
 
-        const io = new Server(httpServer, {
+        this.server = new Server(httpServer, {
             cors: {
                 origin: options.origin,
                 credentials: true
             }
         });
 
-        if (options.debug)
-            instrument(io, {
+        if (options.debug) {
+
+            instrument(this.server, {
                 auth: false
             });
+            let room = this.createRoom("debug")
+            console.log("Room debug: " + room.getId());
+
+        }
 
         httpServer.listen(PORT);
 
+
+
         console.log("Waiting for matchmaker..");
-        io.of('/matchmaker').on("connection", (socket) => {
+        this.server.of('/matchmaker').on("connection", (socket) => {
             if (this.isMatchmakerUp) {
                 console.log("A matchmaker is already connected, but we can communicate with multiple instance, i guess");
             }
@@ -49,7 +58,7 @@ export abstract class GameSocket {
             })
 
             socket.on('createRoom', ({ name }, cb) => {
-                let room = this.rooms.createRoom(name)
+                let room = this.createRoom(name)
                 cb(room.getId())
             })
 
@@ -62,6 +71,17 @@ export abstract class GameSocket {
 
             socket.emit('getRooms', this.rooms.getPublicRooms())
         });
+    }
+
+    protected abstract setupListeners(socket: Socket): void;
+
+    protected createRoom(name: string) {
+        let room = this.rooms.createRoom(name)
+        // setup listeners
+        this.server.of(room.getId()).on('connection', (socket) => {
+            this.setupListeners(socket)
+        })
+        return room
     }
 
     // public removeRoom(id: string) {
