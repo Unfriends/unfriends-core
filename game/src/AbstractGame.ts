7bb8a19349a5ad7287286084fc5b26f754f66c70
Yohan <yohan.g99@gmail.com>
2022-02-10 15:48:52 +0100

    abstract sync state

--- game/src/AbstractGame.ts
+++ game/src/AbstractGame.ts
@@ -12,30 +12,30 @@ export abstract class AbstractGame<Configuration, GameState, Player extends Abst
     /**
      * @return Should return true if the game is over
      */
-    public abstract isGameOver(): boolean
+    public abstract isGameOver (): boolean
     /**
      * @return Get the game configuration according to the number of player
      */
-    public abstract generateConfigAccordingToPlayers(count: number): Configuration;
+    public abstract generateConfigAccordingToPlayers (count: number): Configuration;
     /**
      * @return Call this method to stop the game, with the end reason
      */
-    protected abstract stop(data?: any): void;
+    protected abstract stop (data?: any): void;
     /**
      * @return Called on game start. Init all game infos here
      */
-    protected abstract onInitGame(): void;
+    protected abstract onInitGame (): void;
     /**
      * @return Get a representation of the state of each player at the end of the game
      */
-    public abstract getLeaderboard(): any[]
+    public abstract getLeaderboard (): any[]
 
     /**
      * Get a player from his ID
      * @param id Id of player
      * @returns Player, or throw an error
      */
-    public getPlayer(id: string): Player {
+    public getPlayer (id: string): Player {
         let player = this.players.find(p => p.getId() === id)
         if (!player)
             throw new Error(`Player not found: ${id}`)
@@ -44,20 +44,20 @@ export abstract class AbstractGame<Configuration, GameState, Player extends Abst
     /**
      * @description List of players present in game
     */
-    public getPlayers() {
+    public getPlayers () {
         return this.players;
     }
     /**
      * @description List of players, with private informations
      */
-    public getPlayersPrivateInfos() {
+    public getPlayersPrivateInfos () {
         return this.players.map(p => { return { id: p.getId(), infos: p.getPrivateInfos() } })
     }
 
     /**
      * @description List of players, with public informations
      */
-    public getPlayersInfos() {
+    public getPlayersInfos () {
         return this.players.map(p => { return { id: p.getId(), infos: p.getPublicInfos() } })
     }
 
@@ -65,7 +65,7 @@ export abstract class AbstractGame<Configuration, GameState, Player extends Abst
      * @description Get private infos of a player
      * @param id Id of player
      */
-    public getPlayerPrivateInfosFromId(id: string) {
+    public getPlayerPrivateInfosFromId (id: string) {
         return this.getPlayer(id).getPrivateInfos()
     }
 
@@ -73,7 +73,7 @@ export abstract class AbstractGame<Configuration, GameState, Player extends Abst
      * Call this method to start the game
      * @param players Player who will play
      */
-    public start(players: PlayerData[]) {
+    public start (players: PlayerData[]) {
         this.players = players.map(p => new this.playerType(p))
         this.onInitGame()
     }
@@ -82,21 +82,20 @@ export abstract class AbstractGame<Configuration, GameState, Player extends Abst
     /**
      * @return Return current game configuration
      */
-    public getConfiguration(): Configuration {
+    public getConfiguration (): Configuration {
         return this.configuration
     }
 
     /**
      * Set game config. You can set your own, or use @generateConfigAccordingToPlayers() method to generate a configuration
      */
-    public setConfiguration(config: Configuration) {
+    public setConfiguration (config: Configuration) {
         return this.configuration = config
     }
 
     /**
-     * @return Return current game state, or false if we're in lobby
+     * Players are in state by default
+     * @returns Public state game
      */
-    public getState(): GameState | false {
-        return this.gameState
-    }
+    abstract getState (): GameState;
 }
