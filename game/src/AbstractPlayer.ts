9e89170da5a081006de6a99c4234233b699fb9c4
Yohan <yohan.g99@gmail.com>
2022-02-28 15:49:03 +0100

    success management

--- game/src/AbstractPlayer.ts
+++ game/src/AbstractPlayer.ts
@@ -1,10 +1,10 @@
 import { PlayerData } from "@unfriends/utils";
+import axios from "axios";
 
 export abstract class AbstractPlayer {
 
 
     constructor(protected data: PlayerData) {
-
     }
 
     /**
@@ -23,5 +23,24 @@ export abstract class AbstractPlayer {
 
     public getId() {
         return this.data.id
+    }
+
+    public async giveSuccess(key: string){
+        try {
+            //Check if user has success
+            let checkReq = await axios.get(`${process.env.API_URL}/api/user/${this.getId()}/success/${key}`)
+            console.log(checkReq.data);
+            
+            if(checkReq.data == true){
+                return false
+            }
+
+            await axios.post(`${process.env.API_URL}/api/user/${this.getId()}/success/${key}`, {}, {headers: {
+                'apiKey': process.env.API_PRIVILEGED_KEY || 'API_PRIVILEGED_KEY env isn\'t set'
+            }})
+            return true
+        } catch (error) {
+            return false
+        }
     }
 }
