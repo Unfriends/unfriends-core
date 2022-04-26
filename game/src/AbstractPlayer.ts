a07a1e72a8f30c56d5a1842a8efb36307abf2212
Eteckq <yohan.g99@gmail.com>
2022-04-26 10:55:11 +0200

    give success dev

--- game/src/AbstractPlayer.ts
+++ game/src/AbstractPlayer.ts
@@ -10,31 +10,36 @@ export abstract class AbstractPlayer {
     /**
      * @description Get private infos for a player
      */
-    abstract getPrivateInfos(): any;
+    abstract getPrivateInfos (): any;
 
     /**
      * @description Get public infos for a player
      */
-    abstract getPublicInfos(): any;
+    abstract getPublicInfos (): any;
 
-    public getData() {
+    public getData () {
         return this.data
     }
 
-    public getId() {
+    public getId () {
         return this.data.id
     }
 
-    public async giveSuccess(key: string){
+    public async giveSuccess (key: string) {
+        if (process.env.ENV === 'dev') {
+            return true
+        }
         try {
             //Check if user has success
             let checkReq = await axios.get(`${process.env.API_URL}/api/user/${this.getId()}/success/${key}`)
-            if(checkReq.data == true){
+            if (checkReq.data == true) {
                 return false
             }
-            await axios.post(`${process.env.API_URL}/api/user/${this.getId()}/success/${key}`, {}, {headers: {
-                'apiKey': process.env.API_PRIVILEGED_KEY || 'API_PRIVILEGED_KEY env isn\'t set'
-            }})
+            await axios.post(`${process.env.API_URL}/api/user/${this.getId()}/success/${key}`, {}, {
+                headers: {
+                    'apiKey': process.env.API_PRIVILEGED_KEY || 'API_PRIVILEGED_KEY env isn\'t set'
+                }
+            })
             return true
         } catch (error) {
             return false
